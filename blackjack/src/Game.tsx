3a3f0bf33ecdfd48cbe6f1c9659b633a43f207f2
BuildTools <unconfigured@null.spigotmc.org>
2026-03-06 10:35:25 -0500

    Card and hand components work but hand is vertical

--- blackjack/src/Game.tsx
+++ blackjack/src/Game.tsx
@@ -1,9 +1,13 @@
 import Quotes from "./Quotes.tsx";
+import Card from "./Card.tsx";
+import Hand from "./Hand.tsx";
 
 export default function Game() {
     return (
         <>
         <Quotes />
+        // uses props to control a card's values
+        <Hand />
         </>
     )
 }
